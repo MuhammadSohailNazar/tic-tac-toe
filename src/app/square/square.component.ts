f5413db3a0334a036a763cab03ddac17d4ebe8cc
Muhammad Sohail Nazar <sohail@tcig.co>
2021-08-26 20:59:07 +0300

    adding firebase adding pwa

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -14,5 +14,5 @@ import { Component, Input } from '@angular/core';
   styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }'],
 })
 export class SquareComponent {
-  @Input() value: 'X' | 'O' | undefined;
+  @Input() value: undefined;
 }
